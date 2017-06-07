43e215e5db41e3beb1d5bcd1c069529bd832767b
aerth <aerth@riseup.net>
2017-06-07 02:05:24 +0000

    add thunix plugin

---
+++ thunix/thunix.go
@@ -0,0 +1,40 @@
+// +build cgo
+
+// Skeleton plugin adds "plugin-test" public command to ircb
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/aerth/ircb"
+)
+
+// empty main
+func main() {}
+
+// Init must be named Init taking an ircb.Connection and returning an error
+// Useful methods of ircb.Connection include:
+//  c.AddCommand(name, fn)
+//  c.AddMasterCommand(name, fn)
+//  c.RemoveCommand(name)
+//  c.RemoveMasterCommand(name)
+//  c.SendMaster(format, ...interface{})
+func Init(c *ircb.Connection) error {
+	name := "thunar" // no global variables in plugin
+	c.Log.Println("plugin loading:", name)
+	c.AddCommand("shells", commandShells)
+	c.Log.Println("plugin loaded:", name)
+	return nil
+}
+
+func commandShells(c *ircb.Connection, irc *ircb.IRC) {
+	dir, err := ioutil.ReadDir("/home")
+	if err != nil {
+		c.Log.Println(err)
+		return
+	}
+
+	irc.Reply(c, fmt.Sprintf("Current number of shell accounts: %v", len(dir)))
+
+}
